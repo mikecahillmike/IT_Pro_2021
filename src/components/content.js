9fd3f1dc5fa24e4dad9e64f4417677b5fae2c4fe
Albert Anderson <G00372258@gmit.ie>
2021-02-22 16:59:03 +0000

    Nav bar header component footer

---
+++ src/components/content.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class Content extends React.Component{
+
+    render(){
+        return(
+            <div>
+               <h1>Eire Car Sales</h1>
+            </div>
+        );
+    }
+
+}
