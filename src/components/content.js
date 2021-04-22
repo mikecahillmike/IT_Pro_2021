9708e2489ecb3c742b9a8c88e65779955fe2a328
albertanderson <48316595+albertanderson@users.noreply.github.com>
2021-04-22 11:44:01 +0100

    Add files via upload
    Home page

--- src/components/content.js
+++ src/components/content.js
@@ -2,12 +2,32 @@ import React from 'react';
 
 export class Content extends React.Component{
 
-    render(){
-        return(
-            <div>
-               <h1>Eire Car Sales</h1>
+  render(){
+    const mystyle = { 
+        color: "black",
+        backgroundColor: "red",
+        padding: "40px",
+        fontFamily: "Verdana"
+        
+      };
+      return (
+        <div>
+        <h1 style={mystyle}>Eire Car Sales</h1>
+        <h4>Find your perfect car</h4>
+       <br></br>
+       <p>Ready to fall in love? You don’t need to wait months to find the perfect match with brand-new cars available to drive away today.</p>
+              <div style={{ 
+        backgroundImage: `url(${process.env.PUBLIC_URL + 'https://m.atcdn.co.uk/ect/media/w1024/e298db098d904528b1325b5fa7e93402.jpg'})`, 
+        
+        backgroundPosition: 'center',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        width:'100vw',
+        height:'100vh' 
+        }}>
             </div>
-        );
+            </div>
+        
+      );
     }
-
-}
+  }
